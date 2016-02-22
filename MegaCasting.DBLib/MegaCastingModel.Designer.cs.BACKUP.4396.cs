db1a52c3aa128de20791226a9b0e49fc8710c106
Marteea <martin.amicel@gmail.com>
2016-02-22 20:20:26 +0100

    ERROR CONFLICT
    COMMENT : Tentative de résolution conflit avec Kdiff3

---
+++ MegaCasting.DBLib/MegaCastingModel.Designer.cs.BACKUP.4396.cs
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+﻿// La génération de code T4 est activée pour le modèle 'C:\Users\PC-2016-01\Documents\Projet PPE\ApplicationLourde\MegaCasting.DBLib\MegaCastingModel.edmx'. 
+// Pour activer la génération de code héritée, définissez la valeur de la propriété
+// du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
+//  est ouvert dans le concepteur.
+=======
+﻿// T4 code generation is enabled for model 'C:\Users\Radouane\Desktop\Radouane\Cours-IIA\PPE\ApplicationLourde\MegaCasting.DBLib\MegaCastingModel.edmx'. 
+// To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
+// property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
+// is open in the designer.
+>>>>>>> 3dea635ded03e1afeff53780ae401e89c52122e7
+
+// If no context and entity classes have been generated, it may be because you created an empty model but
+// have not yet chosen which version of Entity Framework to use. To generate a context class and entity
+// classes for your model, open the model in the designer, right-click on the designer surface, and
+// select 'Update Model from Database...', 'Generate Database from Model...', or 'Add Code Generation
+// Item...'.
