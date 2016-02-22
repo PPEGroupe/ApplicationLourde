3dea635ded03e1afeff53780ae401e89c52122e7
Bouzerrara Radouane <Bouzerrara Radouane>
2016-02-22 20:08:16 +0100

    UPDATEFILES ClientWindow.xaml, ClientWindows.xaml.cs IN MegaCasting. COMMENT : Affichage des informations client dans les champs.

--- MegaCasting/ClientWindow.xaml.cs
+++ MegaCasting/ClientWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MegaCasting.DBLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,18 +20,36 @@ namespace MegaCasting
     /// </summary>
     public partial class ClientWindow : Window
     {
+        private MegaCastingEntities db = new MegaCastingEntities();
+
         public ClientWindow()
         {
             InitializeComponent();
+
+            Client client = new Client();
+
+            client.Identifier = 00222;
+            client.ZipCode = "53000";
+            client.PhoneNumber = "03165986532";
+            client.URL = "www.client.com";
+            client.Address = "39 rue franche comté 53000";
+            client.City = "Laval";
+            client.Email = "client@client.com";
+            client.Fax = "0649";
+
+            this.DataContext = client;
+
         }
 
         private void ButtonCancel_Click(object sender, RoutedEventArgs e)
         {
+
             this.Close();
         }
 
         private void ButtonValidate_Click(object sender, RoutedEventArgs e)
         {
+            
             this.Close();
         }
     }
