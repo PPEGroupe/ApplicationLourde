66683d96c562b36fa5a39fbc9c2eaf558e14764e
Marteea <martin.amicel@gmail.com>
2016-02-24 19:28:13 +0100

    UPDATEFILE OfferWindow IN MegaCasting.
    COMMENT : Rajout de colonne pour le Zip Code et la City.

--- MegaCasting.DBLib/Client.cs
+++ MegaCasting.DBLib/Client.cs
@@ -28,6 +28,7 @@ namespace MegaCasting.DBLib
         public string Address { get; set; }
         public string City { get; set; }
         public string ZipCode { get; set; }
+        public string Company { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ObservableCollection<Offer> Offer { get; set; }
