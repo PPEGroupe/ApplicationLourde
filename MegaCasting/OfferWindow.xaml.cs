7e357b9c810313e34c322fe704b6cc7c30fa593d
Marteea <martin.amicel@gmail.com>
2016-03-27 13:54:48 +0200

    ADDFILES pin.gif, pinHorizontal.gif IN MegaCasting.
    UPDATEFILES OfferWindow.xaml, OfferWindow.xaml.cs IN MegaCasting.
    COMMENT : Onglet dockable ajouté dans OfferWindow = Fonctionne.
    TODO : Ajouter la carte dans l'onglet dockable et positionner les offres.

--- MegaCasting/OfferWindow.xaml.cs
+++ MegaCasting/OfferWindow.xaml.cs
@@ -22,22 +22,9 @@ namespace MegaCasting
     public partial class OfferWindow : Window
     {
         #region Construction des clones des colonnes
-        // Clones colonnes pour les couches 0 et 1, 2X pour 0 
+        // Clones de colonnes pour les docké sur Layer 0
         ColumnDefinition column1CloneForLayer0;
-        ColumnDefinition column2CloneForLayer0;
-        ColumnDefinition column2CloneForLayer1;
 
-        public OfferWindow()
-        {
-            InitializeComponent();
-            // Initialize the dummy columns used when docking:
-            column1CloneForLayer0 = new ColumnDefinition();
-            column1CloneForLayer0.SharedSizeGroup = "column1";
-            column2CloneForLayer0 = new ColumnDefinition();
-            column2CloneForLayer0.SharedSizeGroup = "column2";
-            column2CloneForLayer1 = new ColumnDefinition();
-            column2CloneForLayer1.SharedSizeGroup = "column2";
-        }
         #endregion
 
         #region Attributs
@@ -52,6 +39,9 @@ namespace MegaCasting
             db = context;
 
             InitializeComponent();
+            // Initialise les clones de colonnes pour docker
+            column1CloneForLayer0 = new ColumnDefinition();
+            column1CloneForLayer0.SharedSizeGroup = "column1";
         }
         #endregion
 
@@ -60,75 +50,29 @@ namespace MegaCasting
         public void Panel1Pin_Click(object sender, RoutedEventArgs e)
         {
             if (ButtonPanel1.Visibility == Visibility.Collapsed)
-                UndockPane(1);
+                UndockPanel(1);
             else
-                DockPane(1);
-        }
-
-        // Basculement entre l'état épinglé et non épinglé du Panel2
-        public void Panel2Pin_Click(object sender, RoutedEventArgs e)
-        {
-            if (ButtonPanel2.Visibility == Visibility.Collapsed)
-                UndockPane(2);
-            else
-                DockPane(2);
+                DockPanel(1);
         }
 
         // Montre le Panel 1 au survol de la souris sur ce boutton
-        private void ButtonPanel1_MouseEnter(object sender, MouseEventArgs e)
+        public void ButtonPanel1_MouseEnter(object sender, MouseEventArgs e)
         {
             Layer1.Visibility = Visibility.Visible;
-
-            // Ajuste pour être sur que le Panel1 est en haut
-            ParentGrid.Children.Remove(Layer1);
-            //ParentGrid.Children.Add(Layer1);
-
-            // Vérifie que l'autre Panel est caché si il n'est pas épinglé
-            if (ButtonPanel2.Visibility == Visibility.Visible)
-                Layer2.Visibility = Visibility.Collapsed;
         }
 
-        // Montre le Panel 2 au survol de la souris sur ce boutton
-        public void ButtonPanel2_MouseEnter(object sender, RoutedEventArgs e)
-        {
-            Layer2.Visibility = Visibility.Visible;
 
-            // Ajuste pour être sur que le Panel2 est en haut
-            ParentGrid.Children.Remove(Layer2);
-            //ParentGrid.Children.Add(Layer2);
-
-            // Vérifie que l'autre Panel est caché si il n'est pas épinglé
-            if (ButtonPanel1.Visibility == Visibility.Visible)
-                Layer1.Visibility = Visibility.Collapsed;
-        }
-
-        // Cache les Panel non épinglés quand la souris entre en Layer 0
+        // Cache le Panel1 Sinon épinglé quand la souris entre en Layer 0
         public void Layer0_MouseEnter(object sender, RoutedEventArgs e)
         {
             if (ButtonPanel1.Visibility == Visibility.Visible)
+            {
                 Layer1.Visibility = Visibility.Collapsed;
-            if (ButtonPanel2.Visibility == Visibility.Visible)
-                Layer2.Visibility = Visibility.Collapsed;
-        }
-
-        // Cache les autres Panel si ils sont non épinglés quand la soiris entre en Panel1
-        public void Panel1_MouseEnter(object sender, RoutedEventArgs e)
-        {
-            // S'assure que l'autre Panel est caché si non épinglé
-            if (ButtonPanel2.Visibility == Visibility.Visible)
-                Layer2.Visibility = Visibility.Collapsed;
-        }
-
-        // Cache les autres Panel si ils sont non épinglés quand la soiris entre en Panel2
-        public void Panel2_MouseEnter(object sender, RoutedEventArgs e)
-        {
-            // S'assure que l'autre Panel est caché si non épinglé
-            if (ButtonPanel1.Visibility == Visibility.Visible)
-                Layer1.Visibility = Visibility.Collapsed;
-        }
+            }                
+        } 
 
         // Epingle un Panel, qui cache le ButtonPanel correspondant 
-        public void DockPane(int paneNumber)
+        public void DockPanel(int paneNumber)
         {
             if (paneNumber == 1)
             {
@@ -137,23 +81,11 @@ namespace MegaCasting
 
                 // Ajoute la colonne clonée au Layer 0
                 Layer0.ColumnDefinitions.Add(column1CloneForLayer0);
-                // Ajoute le clone de la colonne de Layer1, seulement si le Panel2 est épinglé
-                if (ButtonPanel2.Visibility == Visibility.Collapsed) Layer1.ColumnDefinitions.Add(column2CloneForLayer1);
-            }
-            else if (paneNumber == 2)
-            {
-                ButtonPanel2.Visibility = Visibility.Collapsed;
-                Panel2PinImage.Source = new BitmapImage(new Uri("pin.gif", UriKind.Relative));
-
-                // Add the cloned column to layer 0:
-                Layer0.ColumnDefinitions.Add(column2CloneForLayer0);
-                // Add the cloned column to layer 1, but only if pane 1 is docked:
-                if (ButtonPanel1.Visibility == Visibility.Collapsed) Layer1.ColumnDefinitions.Add(column2CloneForLayer1);
             }
         }
 
         // Désépingle un Panel, qui cache le ButtonPanel correspondant
-        public void UndockPane(int paneNumber)
+        public void UndockPanel(int paneNumber)
         {
             if (paneNumber == 1)
             {
@@ -163,19 +95,6 @@ namespace MegaCasting
 
                 // Retire la colonne clonée au Layer 0 et 1
                 Layer0.ColumnDefinitions.Remove(column1CloneForLayer0);
-                // Retire, si besoin, les mauvaises colonnes
-                Layer1.ColumnDefinitions.Remove(column2CloneForLayer1);
-            }
-            else if (paneNumber == 2)
-            {
-                Layer2.Visibility = Visibility.Collapsed;
-                ButtonPanel2.Visibility = Visibility.Visible;
-                Panel2PinImage.Source = new BitmapImage(new Uri("pinHorizontal.gif", UriKind.Relative));
-
-                // Remove the cloned columns from layers 0 and 1:
-                Layer0.ColumnDefinitions.Remove(column2CloneForLayer0);
-                // This won't always be present, but Remove silently ignores bad columns:
-                Layer1.ColumnDefinitions.Remove(column2CloneForLayer1);
             }
         }
         #endregion
@@ -225,5 +144,7 @@ namespace MegaCasting
         }
 
         #endregion
+
+        
     }
 }
